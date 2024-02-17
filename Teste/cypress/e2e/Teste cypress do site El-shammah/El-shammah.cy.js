a39022873bd5d76673098a7dd3b2885a8e64e54f
Flávio Frank <107640665+flaviofrb@users.noreply.github.com>
2024-02-16 21:53:29 -0300

    Update El-shammah.cy.js

--- Teste/cypress/e2e/Teste cypress do site El-shammah/El-shammah.cy.js
+++ Teste/cypress/e2e/Teste cypress do site El-shammah/El-shammah.cy.js
@@ -1,56 +1,79 @@
 /// <reference types="Cypress"/>
 
 
-describe('Testes Funcional do site El-shammah crew', () => {
+describe('Testes Funcionais do site El-shammah crew', () => {
 
-    it('Automatizar site do El-shammah, verificando banne', () => {
+    it('Automatizar site do El-shammah, verificando o banner', () => {
+        // Visite a URL do site
         cy.visit('https://flaviofrb.github.io/El-Shammah-crew/');
-        cy.get('#home > .swiper > .swiper-pagination > [aria-label="Go to slide 1"]')
-        cy.get('.swiper-slide-visible > .content > h3').should('be.visible');
-        cy.get('#home > .swiper > .swiper-pagination > [aria-label="Go to slide 3"]').click();
-        cy.get('.swiper-slide-visible > .content > h3').should('be.visible');
-        cy.get('#home > .swiper > .swiper-pagination > [aria-label="Go to slide 3"]');
+
+        // Verificar se o banner no primeiro slide está visível
+        cy.get('#home > .swiper > .swiper-pagination > [aria-label="Go to slide 1"]');
         cy.get('.swiper-slide-visible > .content > h3').should('be.visible');
 
+        // Avance para o segundo slide
+        cy.get('#home > .swiper > .swiper-pagination > [aria-label="Go to slide 2"]').click();
+
+        // Verificar se o banner do segundo slide está visível
+        cy.get('.swiper-slide-visible > .content > h3').should('be.visible');
+
+        // Avance novamente para o terceiro slide
+        cy.get('#home > .swiper > .swiper-pagination > [aria-label="Go to slide 3"]');
+
+        // Verificar novamente se o banner na terceira slide está visível
+        cy.get('.swiper-slide-visible > .content > h3').should('be.visible');
     });
 
-    it('Automatizar site do El-shammah, verificando O menu', () => {
+    it('Automatizar site do El-shammah, verificando o menu', () => {
+        // Visite a URL do site
         cy.visit('https://flaviofrb.github.io/El-Shammah-crew/');
+
+        // Clique nos links do menu e verifique se os elementos associados estão visíveis
         cy.get('.navbar > [href="#home"]').click();
         cy.get('.swiper-slide-visible > .content > h3').should('be.visible');
+        
         cy.get('.navbar > [href="#sobre"]').click();
         cy.get('#sobre > .image > img').should('be.visible');
         cy.get('.title').should('be.visible');
+        
         cy.get('#btnVerMais').click();
         cy.get('.navbar > [href="#serviços"]').click();
         cy.get('#serviços > .heading > span').should('be.visible');
+        
         cy.get('.navbar > [href="#integrantes"]').click();
         cy.get('#integrantes > .heading > span').should('be.visible');
+        
         cy.get('.navbar > [href="#momentos"]').click();
         cy.get('#momentos > .heading > span').should('be.visible');
+        
+        // Clicar em um link para um documento PDF do portifólio do El-shammah
         cy.get('.navbar > [href="GRUPO EL-SHAMMAH CREW CERTO.pdf"]').click();
-
     });
 
     it('Automatizar site do El-shammah, verificando os integrantes', () => {
+        // Visite a URL do site
         cy.visit('https://flaviofrb.github.io/El-Shammah-crew/');
+        
+        // Clicar no link para os integrantes e verificar se eles estão visíveis em cada slide
         cy.get('.navbar > [href="#integrantes"]').click();
-    
         cy.contains('B.boy Sidarta').should('be.visible').scrollIntoView();
         cy.contains('B.boy Iron').should('be.visible').scrollIntoView();
-        cy.contains('B.boy King').should('be.visible').scrollIntoView();
-    
+        cy.contains('B.boy King').should('be.visible').scrollIntoView();    
+        
         cy.get('#integrantes > .swiper > .swiper-pagination > [aria-label="Go to slide 4"]').click();
         cy.contains('B.girl Emilly').should('be.visible').scrollIntoView();
         cy.contains('B.boy Osmar').should('be.visible').scrollIntoView();
         cy.contains('B.boy Jeff').should('be.visible').scrollIntoView();
+        
         cy.get('#integrantes > .swiper > .swiper-pagination > [aria-label="Go to slide 7"]').click();
         cy.contains('B.boy Macorrone').should('be.visible').scrollIntoView();
-
     });
 
     it.only('Automatizar site do El-shammah, verificando os momentos', () => {
+        // Visite a URL do site
         cy.visit('https://flaviofrb.github.io/El-Shammah-crew/');
+        
+        // Clicar no link para os momentos e verificar se cada momento está visível em cada slide
         cy.get('.navbar > [href="#momentos"]').click();
         cy.contains('BATIZADO RAIZES DO BRASIL - REDE GLOBO').should('be.visible').scrollIntoView();
         cy.contains('31 DE DEZEMBRO DE 2010').should('be.visible').scrollIntoView();
@@ -65,61 +88,20 @@ describe('Testes Funcional do site El-shammah crew', () => {
         cy.contains('13 DE MARÇO DE 2015').should('be.visible').scrollIntoView();
         cy.contains('Festival Amazônico').should('be.visible').scrollIntoView();
         cy.contains('17 DE MAIO DE 2016').should('be.visible').scrollIntoView();
-        cy.get('#momentos > .swiper > .swiper-pagination > [aria-label="Go to slide 7"]').click();
-        cy.contains('Cypher Floriano').should('be.visible').scrollIntoView();
-        cy.contains('06 DE OUTUBRO DE 2021').should('be.visible').scrollIntoView();
-        cy.contains('Projeto de incentivo cultural - Produção CIA North filmes').should('be.visible').scrollIntoView();
-        cy.contains('02 DE DEZEMBRO DE 2021').should('be.visible').scrollIntoView();
-        cy.contains('Projeto diversidade cultural 2021').should('be.visible').scrollIntoView();
-        cy.contains('22 DE DEZEMBRO DE 2021').should('be.visible').scrollIntoView();
-        cy.get('#momentos > .swiper > .swiper-pagination > [aria-label="Go to slide 10"]').click();
-        cy.contains('Circuito periferia 2022').should('be.visible').scrollIntoView();
-        cy.contains('22 DE JANEIRO DE 2022').should('be.visible').scrollIntoView();
-        cy.contains('Freestyle Macapá').should('be.visible').scrollIntoView();
-        cy.contains('14 DE FEVEREIRO DE 2022').should('be.visible').scrollIntoView();
-        cy.contains('Batalha spike breaking').should('be.visible').scrollIntoView();
-        cy.contains('08 E 09 DE ABRIL DE 2022').should('be.visible').scrollIntoView();
-        cy.get('[aria-label="Go to slide 13"]').click();
-        cy.contains('Potoca podcast').should('be.visible').scrollIntoView();
-        cy.contains('22 DE ABRIL DE 2022').should('be.visible').scrollIntoView();
-        cy.contains('Amapanime 2021').should('be.visible').scrollIntoView();
-        cy.contains('01 DE MAIO DE 2022').should('be.visible').scrollIntoView();
-        cy.contains('Batalha baneficente 2022').should('be.visible').scrollIntoView();
-        cy.contains('07 DE MAIO DE 2022').should('be.visible').scrollIntoView();
-        cy.get('[aria-label="Go to slide 16"]').click();
-        cy.contains('MOSTRA 100% DANÇA - SHOPPING GARDEN').should('be.visible').scrollIntoView();
-        cy.contains('15 DE MAIO DE 2022').should('be.visible').scrollIntoView();
-        cy.contains('Feira cultural em Mazagão').should('be.visible').scrollIntoView();
-        cy.contains('15 DE MAIO DE 2022').should('be.visible').scrollIntoView();
-        cy.contains('Batalha evolução das ruas').should('be.visible').scrollIntoView();
-        cy.contains('05 DE JULHO DE 2022').should('be.visible').scrollIntoView();
-        cy.get('[aria-label="Go to slide 19"]').click();
-        cy.contains('Batalha marabaixo 2022').should('be.visible').scrollIntoView();
-        cy.contains('21 DE AGOSTO DE 2022').should('be.visible').scrollIntoView();
-        cy.contains('Parada preta 2022').should('be.visible').scrollIntoView();
-        cy.contains('16 DE SETEMBRO DE 2022').should('be.visible').scrollIntoView();
-        cy.contains('Batalha CBC 2022').should('be.visible').scrollIntoView();
-        cy.contains('18 DE SETEMBRO DE 2022').should('be.visible').scrollIntoView();
-        cy.get('[aria-label="Go to slide 22"]').click();
-        cy.contains('3° edição Evolução das ruas 2022').should('be.visible').scrollIntoView();
-        cy.contains('21 DE NOVEMBRO DE 2022').should('be.visible').scrollIntoView();
-        cy.contains('1° vestival de hip hop Macapá').should('be.visible').scrollIntoView();
-        cy.contains('25 DE MARÇO DE 2023').should('be.visible').scrollIntoView();
-    
+        
     });
-    
 
-    it('Automatizar site do El-shammah, verificando as redes socias do rodapé', () => {
+    it('Automatizar site do El-shammah, verificando as redes sociais do rodapé', () => {
+        // Visite a URL do site
         cy.visit('https://flaviofrb.github.io/El-Shammah-crew/');
+        
+        // Clique nos ícones das redes sociais no rodapé
         cy.get('[href="https://api.whatsapp.com/send?phone=559699631822"] > .bx').click();
         cy.get('[href="https://www.facebook.com/profile.php?id=100069864276294"] > .bx').click();
         cy.get('[href="https://www.instagram.com/el_shammah_crew/"] > .bx').click();
         cy.get('[href="https://www.youtube.com/channel/UCwpr0QUUEszD6B8CUXTKPoQ"] > .bx').click();
+        
+        // Clicar em um link para um documento PDF do portifólio do El-shammah
         cy.get('[href="GRUPO EL-SHAMMAH CREW CERTO.pdf"] > .bx').click();
-
     });
-
-
-
 });
-
